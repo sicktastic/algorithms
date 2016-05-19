b499c14a8b42793f413b4a0f0f8cabe01a88ec19
Anthony Lee <antwonlee@gmail.com>
2016-05-18 21:09:29 -0500

    Add sense function

---
+++ localization/sense_function.py
@@ -0,0 +1,14 @@
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    return q
+
+print sense(p, Z)
