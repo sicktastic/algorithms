6d0165606117387ccd0e8dfba32a05812633707a
Anthony Lee <antwonlee@gmail.com>
2016-05-19 00:51:33 -0500

    Add multiple measurement :clap:
    https://www.udacity.com/course/viewer#!/c-cs373/l-48739381/e-48739380/m-48722172

---
+++ localization/multiple_measurements.py
@@ -0,0 +1,20 @@
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range (len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+for k in range(len(measurements)):
+    p = sense(p, measurements[k])
+
+print p
