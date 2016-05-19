f0182227fec08d68055b49e1096f895465ffc8e4
Anthony Lee <antwonlee@gmail.com>
2016-05-18 21:15:31 -0500

    Add normalized sense function solution
    Using python loop

---
+++ localization/normalized_sense_function_solution.py
@@ -0,0 +1,17 @@
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+print sense(p, Z)
