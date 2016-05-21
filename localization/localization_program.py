ff9b73f183ff4140620ddbf6785c2b9be6f6d36c
antwonlee <antwonlee@gmail.com>
2016-05-21 14:42:35 -0500

    Fix syntax after instructors note

--- localization/localization_program.py
+++ localization/localization_program.py
@@ -45,15 +45,13 @@
 
 def localize(colors, measurements, motions, sensor_right, p_move):
     # initializes p to a uniform distribution over a grid of the same dimensions as colors
-    def sense(p, colors, measurement):
-
+    def sense(p, colors, measurements, sensor_wrong):
         aux = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
 
         s = 0.0
-
         for i in range(len(p)):
             for j in range(len(p[i])):
-                hit = (measurement == colors[i][j])
+                hit = (measurements == colors[i][j])
                 aux[i][j] = p[i][j] * (hit * sensor_right + (1 - hit) * sensor_wrong)
                 s += aux[i][j]
 
@@ -62,23 +60,24 @@ def localize(colors, measurements, motions, sensor_right, p_move):
                 aux[i][j] /= s
         return aux
 
-    def move(p, motion):
+    def move(p, motions, p_stay):
         aux = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
 
         for i in range(len(p)):
             for j in range(len(p[i])):
-                aux[i][j] = (p_move * p[(i - motion[0]) % len(p)][(j - motion[1]) % len(p[i])]) + (p_stay * p[i][j])
-        return aux
+                aux[i][j] = (p_move * p[(i - motions[0]) % len(p)][(j - motions[1]) % len(p[i])]) + (p_stay * p[i][j])
+            return aux
 
     pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
     p = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
 
+    # >>> Insert your code here <<<
     sensor_wrong = 1.0 - sensor_right
     p_stay = 1.0 - p_move
 
     for k in range(len(measurements)):
-        p = move(p, motions[k])
-        p = sense(p, colors, measurements[k])
+        p = move(p, motions[k], p_stay)
+        p = sense(p, colors, measurements[k], sensor_wrong)
 
     return p
 
@@ -103,5 +102,5 @@ colors = [['R', 'G', 'G', 'R', 'R'],
 
 measurements = ['G', 'G', 'G', 'G', 'G']
 motions = [[0, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
-p = localize(colors, measurements, motions, sensor_right = 0.7, p_move = 0.8)
+p = localize(colors, measurements, motions, sensor_right=0.7, p_move=0.8)
 show(p)  # displays your answer
