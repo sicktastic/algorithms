fa823f2ad0983111fd6451a7f2d863c9a9c0947c
antwonlee <antwonlee@gmail.com>
2016-05-27 09:24:23 -0500

    Add answer to programming exercise

--- kalman_filtering/programming_exercise.py
+++ kalman_filtering/programming_exercise.py
@@ -183,11 +183,25 @@ u = matrix([[0.], [0.], [0.], [0.]])  # external motion
 #### DO NOT MODIFY ANYTHING ABOVE HERE ####
 #### fill this in, remember to use the matrix() function!: ####
 
-P =  # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
-F =  # next state function: generalize the 2d version to 4d
-H =  # measurement function: reflect the fact that we observe x and y but not the two velocities
-R =  # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
-I =  # 4d identity matrix
+P = matrix([[0., 0., 0., 0.],
+            [0., 0., 0., 0.],
+            [0., 0., 1000., 0.],
+            [0., 0., 0., 1000.]])
+
+F = matrix([[1., 0., dt, 0.],
+            [0., 1., 0., dt],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]])
+
+H = matrix([[1., 0., 0., 0.],
+            [0., 1., 0., 0.]])
+
+R = matrix([[0.1, 0.], [0., 0.1]])
+
+I = matrix([[1., 0., 0, 0.],
+            [0., 1., 0., 0.],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]])
 
 ###### DO NOT MODIFY ANYTHING HERE #######
 
