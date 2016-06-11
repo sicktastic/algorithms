044ef7951b6a6c51d7768286b36cb399117a130c
antwonlee <antwonlee@gmail.com>
2016-06-11 11:31:19 -0500

    Add search project :mag:

---
+++ search/first_search_program.py
@@ -0,0 +1,56 @@
+# ----------
+# User Instructions:
+#
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0], # go up
+         [ 0,-1], # go left
+         [ 1, 0], # go down
+         [ 0, 1]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid[0]))]
+    closed[init[0]][init[1]] = 1
+
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    found = False
+    resign = False
+
+    while found is False and resign is False:
+        if len(open) == 0:
+            resign = True
+            print 'fail'
+
+        else:
+
+    return path
