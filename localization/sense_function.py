b9e343b6b7b9d7f505eebfc1c5c674cc50c8b4b5
antwonlee <antwonlee@gmail.com>
2016-07-07 13:40:39 -0500

    Add print to see the values of hit and pHit

--- localization/sense_function.py
+++ localization/sense_function.py
@@ -8,7 +8,10 @@ def sense(p, Z):
     q = []
     for i in range(len(p)):
         hit = (Z == world[i])
+        print hit
+        print pHit
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+        print q
     return q
 
 print sense(p, Z)
