a4fc88ad72c49f2090e8a52c0ad7fcff24d97146
antwonlee <antwonlee@gmail.com>
2016-09-01 21:11:02 -0500

    Add regression introduction with quandl wiki googl

--- sklearn/regression_intro.py
+++ sklearn/regression_intro.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import quandl
 
+# data frame
+
 df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 
 print(df.head())
