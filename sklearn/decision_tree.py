70848151ef359696da71bb2b7195d9c2688267c7
antwonlee <antwonlee@gmail.com>
2016-09-04 19:46:36 -0500

    Add iris dataset

--- sklearn/decision_tree.py
+++ sklearn/decision_tree.py
@@ -1,10 +1,15 @@
-from sklearn import tree
+# from sklearn.datasets import load_iris
+# from sklearn import tree
 
-X = [[0, 0], [1, 1]]
-Y = [0, 1]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
+# X = [[0, 0], [1, 1]]
+# Y = [0, 1]
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(X, Y)
 # prediction = clf.predict([[2., 2.]])
-prediction = clf.predict_proba([[2., 2.]])
+# prediction = clf.predict_proba([[2., 2.]])
 
-print(prediction)
+# print(prediction)
+
+# iris = load_iris()
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(iris.data, iris.target)
